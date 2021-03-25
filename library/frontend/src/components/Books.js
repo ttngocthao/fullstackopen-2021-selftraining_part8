2375e93d23e9cc7bd92180e48376651188d7d51d
ttngocthao <ttngocthao_87@yahoo.com>
2021-03-25 07:53:01 +0000

    8.19 completed

--- library/frontend/src/components/Books.js
+++ library/frontend/src/components/Books.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {useQuery} from '@apollo/client'
 import {ALL_BOOKS} from '../queries'
 
 const Books = (props) => {
-
-  
+  const [showAll,setShowAll]= useState(true)
+  const [filter,setFilter]= useState(null)
   const {data} = useQuery(ALL_BOOKS)
   const books = data ? data.allBooks :[]
 
-  if (!props.show) {
+  const getAllGenres = (books)=>{   
+    //?extract genres from each book
+    // const genres = books.map(b=>[...b.genres])
+    const genres = books.reduce((newArr,currentVal)=>{
+      newArr.push(...currentVal.genres)
+      return newArr
+    },[])
+    
+    //?get rid the duplication
+    const uniqueGenres = new Set(genres)
+    //?return the list without duplication
+    return Array.from(uniqueGenres)
+  }
+
+  const genreFilter =(genre)=>{
+    if(genre){
+      setFilter(genre)
+      setShowAll(false)
+    }else{     
+      setFilter(null)
+      setShowAll(true)
+    }
+  
+  }
+
+  if(!props.show){
     return null
   }
+
+  const booksDisplay = showAll ? books : books.filter(i=>i.genres.indexOf(filter)>-1)
   
   return (
     <div>
@@ -27,7 +54,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {booksDisplay.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -36,6 +63,11 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+
+      <div>
+        {books && getAllGenres(books).map((g,i)=><button key={i} onClick={()=>genreFilter(g)}>{g}</button>)}
+        <button onClick={()=>genreFilter()}>all generes</button>
+      </div>
     </div>
   )
 }
